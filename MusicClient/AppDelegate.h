7455dc0e3bf3479076d0bb39d653e796ebdc71ef
Alexey Boyko <dev.aboyko@gmail.com>
2015-05-21 09:17:28 +0700

    Initial Commit

---
+++ MusicClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicClient
+//
+//  Created by Alexey Boyko on 21.05.15.
+//  Copyright (c) 2015 Alexey Boyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicClient
+//
+//  Created by Alexey Boyko on 21.05.15.
+//  Copyright (c) 2015 Alexey Boyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
